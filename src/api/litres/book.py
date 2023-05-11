b9d51221212a9deb842964ff47bac9832a8cc33f
Rosya-edwica <rosya-8@yandex.ru>
2023-05-11 19:58:36 +0300

    исправил мелкую ошибку

--- src/api/litres/book.py
+++ src/api/litres/book.py
@@ -4,5 +4,5 @@ from database import find_books_by_header, find_books_by_description
 def get_list_of_books(query: str, count: int, language: str = "all") -> list[Book]:
     books = find_books_by_header(query, count, language)
     if len(books) < count:
-        books += find_books_by_description(query, count, language, ignore = [i.Id for i in books])
+        books += find_books_by_description(query, count, language, ignore = [i.id for i in books])
     return books[:count]
