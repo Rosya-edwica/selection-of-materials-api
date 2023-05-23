713a7db146f9b96845c66fda0c44231003795275
Rosya-edwica <rosya-8@yandex.ru>
2023-05-23 12:25:22 +0300

    исправил баг

--- src/api/youtube/config.py
+++ src/api/youtube/config.py
@@ -1,6 +1,6 @@
 import os
 import dotenv
-
+from random import randint
 loaded = dotenv.load_dotenv(".env")
 if not loaded:
     exit("Не удалось прочитать переменные окружения файла .env")
@@ -18,7 +18,6 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
     "accept-language": "ru,en;q=0.9",
 }
-DEFAULT_JSON_FILE = "default.json"
 
 def get_html(query: str) -> str:
     params = {
@@ -29,14 +28,15 @@ def get_html(query: str) -> str:
     return html
 
 def get_youtube_data_by_query(query: str) -> dict:
-    query = query.replace("/", "_")
+    query_filename =f"{randint(1_000_00, 5_000_00)}_{query}.json"
+    
     html = get_html(query)
     scripts = re.findall("<script.*?</script>", html)
     data = scripts[-5]
     json_data = re.sub("<script.*? =|;</script>", "", data)
-    with open(query+DEFAULT_JSON_FILE, encoding="utf-8", mode="w") as file:
+    with open(query_filename, encoding="utf-8", mode="w") as file:
         json.dump(json.JSONDecoder().decode(json_data), file, ensure_ascii=True, indent=2)
 
-    data = json.load(open(query+DEFAULT_JSON_FILE, encoding="utf-8", mode="r"))
-    os.remove(query+DEFAULT_JSON_FILE)
+    data = json.load(open(query_filename, encoding="utf-8", mode="r"))
+    os.remove(query_filename)
     return data
